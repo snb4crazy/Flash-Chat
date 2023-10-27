d0879ae1ba6f66c22588299044be5e18731414e4
Serhii Dymenko <serge.sergeevich@gmail.com>
2023-10-26 20:54:18 -0400

    Set buttons color to white

--- lib/components/welcome_button.dart
+++ lib/components/welcome_button.dart
@@ -21,6 +21,9 @@ class WelcomeButton extends StatelessWidget {
           height: 42.0,
           child: Text(
             title,
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
